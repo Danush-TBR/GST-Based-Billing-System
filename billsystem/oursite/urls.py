71dd2cc802a1425fb5b9d3fa605433288e4f5837
PRAVEEN BABU <praveenbabu.rcse@sece.ac.in>
2021-08-31 16:09:11 +0530

    initial commit

---
+++ billsystem/oursite/urls.py
@@ -0,0 +1,11 @@
+from . import views
+from django.urls import path
+app_name='oursite'
+urlpatterns=[
+    path('',views.home,name='home'),
+    path('login',views.login,name='login'),
+    path('register',views.register,name='register'),
+    path('logout',views.logout,name='logout'),
+    path('addstock',views.addStock,name='addStock'),
+    path('removestock',views.removeStock,name='removeStock')
+]
